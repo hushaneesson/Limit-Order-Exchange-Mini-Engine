f34c5773f46d43aa28a4ca51faaee5a5641960a5
Hushane <hushaneesson@hotmail.com>
2025-12-13 17:20:04 -0500

    initial setup

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Laravel + Vue</h1>
+    <p class="mt-2">App is running 🚀</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
