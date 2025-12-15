a38ed385e98d8a19fef14ffcbe28dfab5c1f8840
Hushane <hushaneesson@hotmail.com>
2025-12-15 11:42:39 -0500

    updated method to accept user to authenticate

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,15 @@
 namespace Tests;
 
 use App\Models\User;
-use Laravel\Sanctum\Sanctum;
-use Tests\CreatesApplication;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function signIn()
+    public function signIn($user = null)
     {
-        $user = User::where('email', 'test@example.com')->first();
+        $user = $user ?? User::where('email', 'test@example.com')->first();
 
         // Authenticate as this user
         $this->actingAs($user);
-
     }
 }
