154604cf7fb0ce29d06201cd851a7c327d09c2a4
Hushane <hushaneesson@hotmail.com>
2025-12-15 11:38:46 -0500

    removed unnecessary import

--- tests/Feature/NewBuyOrderTest.php
+++ tests/Feature/NewBuyOrderTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class NewBuyOrderTest extends TestCase
--- tests/Feature/NewSellOrderTest.php
+++ tests/Feature/NewSellOrderTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Asset;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class NewSellOrderTest extends TestCase
